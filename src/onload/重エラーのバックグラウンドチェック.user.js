0df2712136d3e5f2c97b4104362f3269d1324650
ITO Tetsunosuke <tetsunosuke.ito@photocreate.co.jp>
2021-11-22 16:54:17 +0900

    勤労の獅子エラーチェック・ジョブカン給与検索を追加 (#10)
    * リポジトリの変更に伴う修正
    
    * テストケースの拡充、カバレッジの修正
    
    * リリース版ビルドとビルドコマンドの修正
    
    * ジョブカン給与検索を追加（単独スクリプトでライブラリを利用しない形にした）
    
    * 勤労の獅子重エラーチェック

---
+++ src/onload/重エラーのバックグラウンドチェック.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         [勤労の獅子]重エラーのバックグラウンドチェック
+// @namespace    https://kinrou.sas-cloud.jp/kinrou/
+// @version      1.0
+// @description 勤労の獅子の当月勤怠データから重エラーを検知します
+// @author       ITO Tetsunosuke
+// @match      https://kinrou.sas-cloud.jp/kinrou/kojin/kintaiDakoku/
+// @grant        GM_log
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const url = "/kinrou/kojin/kojinbetuSyoukai/";
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: url,
+        onload: function(response) {
+            const _document = new DOMParser().parseFromString(response.responseText, "text/html");
+            // hidari_kotei のtrの中で ..big_error_color であればそのindex、を取る
+            // 重エラーのある日々
+            // TODO: 本日分は無視していいかも
+            const hasErrorsIndexList = [..._document.querySelectorAll("#hidari_kotei tbody tr")].reduce( (ac, cv, index) => {
+                // 当日の重エラーは無視
+                if (index === new Date().getDate()) {
+                    return ac;
+                }
+                if (cv.classList.contains("big_error_color")) {
+                    ac.push(index);
+                }
+                return ac;
+            },[]);
+
+            // それらに基づいて日付、曜日、重エラーの内容を取得する
+            const errors = hasErrorsIndexList.map( (index) => {
+                const hidari = _document.querySelector(`#hidari_kotei tbody tr:nth-of-type(${index+1})`);
+                const data_hyouji = _document.querySelector(`#data_hyouji tbody tr:nth-of-type(${index+1})`);
+                const m = {
+                    "date_ymd": hidari.querySelector("#id_date_ymd").innerText,
+                    "week_number": hidari.querySelector("#id_week_number").innerText,
+                    "big_err_contents": data_hyouji.querySelector("#id_big_err_contents").innerText
+                };
+                return `${m.date_ymd} (${m.week_number}） ${m.big_err_contents}`;
+            });
+            // 画面表示
+            const tbody = document.querySelector(".login_data_field table tbody");
+            errors.forEach( (error) => {
+                const tr = document.createElement("tr");
+                const td = document.createElement("td");
+                td.style.textAlign = "left";
+                td.style.color = "#F00";
+                td.style.fontWeight = "bold";
+                td.innerText = error;
+                tr.appendChild(td);
+                tbody.appendChild(tr);
+            });
+        }
+    });
+})();
